532433be37d3701d58a1cd5940af3d05ee157bf2
stas-shr <stasshr@gmail.com>
2023-05-02 20:37:09 +0300

    Initial Commit

---
+++ Marathon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon
+//
+//  Created by Стас on 02.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
